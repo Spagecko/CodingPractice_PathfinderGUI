03f6d15e9e6d3df63a2d5fc620d70732051042e1
Spagecko <bennettlawrenz@gmail.com>
2020-01-27 21:00:18 -0800

    Add files via upload
    Initial Commit. Project has GUI Display Working. 
    Issues:
    Still working on the Visualization  of the BFS algorithm. 
    Reset button does not work 
    Completed:
    The Display 
    Setting walls, Start location and End location

---
+++ Functions.py
@@ -0,0 +1,254 @@
+class cell:
+    row = 0 
+    col = 0
+    isWalled = False
+    visted = False
+    colorState = 0;
+    isStartingPoint =  False
+    isEndingPoint = False
+
+    def __init__(self):
+        row = 0 
+        col = 0
+        isWalled = False
+        visted = False
+        colorState = 0;
+        isStartingPoint =  False
+        isEndingPoint = False
+
+import time
+import collections
+import pygame
+
+def StartState(gridState):
+     for row in range(20):
+         for column in range(33):
+             if(gridState[row][column].isStartingPoint  == True):
+                 return True
+     return False
+
+def Inti2d(gridState):
+     for row in range(20):
+         for column in range(33):
+              gridState[row][column].row = row
+              gridState[row][column].col = column
+
+def ReturnStartState(gridState):
+     for row in range(20):
+         for column in range(33):
+             if(gridState[row][column].isStartingPoint  == True):
+              
+               return row, column
+    
+     return 0
+
+
+                 
+     
+
+def EndState(gridState):
+     for row in range(20):
+         for column in range(33):
+             if(gridState[row][column].isEndingPoint  == True):
+                 return True
+     return False
+
+
+
+
+
+#intilizes the grid
+def intiGrid(grid, gridState,screen):
+    for row in range(20):
+    # Add an empty array that will hold each cell
+    # in this row
+        grid.append([])
+        for column in range(33):
+            grid[row].append(0) 
+    updateGrid(grid,screen)   
+    gridState =[[cell() for j  in range(33) ] for i in range(20)]
+    Inti2d(gridState) # sets the rows and cols 
+#updates the graphic of the grid
+def updateGrid(grid,screen):
+    WIDTH = 15
+    HEIGHT = 15
+    MARGIN = 3
+    GREY = (169, 169, 169)
+    PURPLE = (160, 13, 173)
+    YELLOWGREEN = (153,186,0)
+    GREEN = (0, 255, 0)
+
+    for row in range(20):
+        for column in range(33):
+            WHITE = (255, 255, 255)
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREY
+            if grid[row][column] == 2:
+                color = PURPLE
+            if grid[row][column] == 3:
+                color = YELLOWGREEN
+            if grid[row][column] == 4:
+                color = GREEN
+            if grid[row][column] == 5:
+                color = RED
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+#basic button template
+
+
+def button(gameDisplay,msg,x,y,w,h,ic,ac,):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+  #  if click[0] == 1 and action != None:
+  #          action()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface  = myfont.render(msg, False ,( 0,0,0))
+    gameDisplay.blit(textsurface,(x,y))
+# buttons that will trigger a action
+
+#each button will require different parameters 
+
+#this function will prompt the user to choose Start which will be colored  orange 
+def Startbutton (event,grid, gamestate ):
+    WIDTH = 15
+    HEIGHT = 15
+    MARGIN = 3
+    print("Choose a starting pos")
+    done =  False
+    if( StartState(gamestate) == False):
+        while not done:
+         for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # User clicks the mouse. Get the position
+                pos = pygame.mouse.get_pos()
+                # Change the x/y screen coordinates to grid coordinates
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                # Set that location to one
+            
+                if ( column < 33 and row < 20): # cheecks if the click is within the constrints of the grid 
+
+                    if(grid[row][column] == 0 and gamestate[row][column].isWalled == False ): # making the walls toggable 
+                        grid[row][column] = 2
+                        gamestate[row][column].isStartingPoint = True
+                        
+                        
+                        done = True
+                    
+
+                    else:
+                        print("invalid start")
+                    
+                        done = True
+    else:
+        print("can't set starting point")
+
+         
+  
+ #this function will prompt the user to choose Start which will be colored  Purple 
+def Endbutton (event,grid, gamestate ):
+    WIDTH = 15
+    HEIGHT = 15
+    MARGIN = 3
+    print("Choose a ending pos")
+    done =  False
+    if( EndState(gamestate) == False):
+        while not done:
+         for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # User clicks the mouse. Get the position
+                pos = pygame.mouse.get_pos()
+                # Change the x/y screen coordinates to grid coordinates
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                # Set that location to one
+            
+                if ( column < 33 and row < 20): # cheecks if the click is within the constrints of the grid 
+
+                    if(grid[row][column] == 0 and gamestate[row][column].isWalled == False ): # making the walls toggable 
+                        grid[row][column] = 3
+                        gamestate[row][column].isEndingPoint = True
+                        
+                        
+                        done = True
+                    
+
+                    else:
+                        print("invalid Ending")
+                    
+                        done = True
+    else:
+        print("can't set Ending point")
+  
+ #this function will reset the state of the gameboard to all 0s  
+def Resetbutton( grid, gamestate,screen):
+    intiGrid(grid,gamestate,screen)
+
+
+
+def BFSbutton(gameDisplay,msg,x,y,w,h,ic):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if click[0] == 1 and action != None:
+            action()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface  = myfont.render(msg, False ,( 0,0,0))
+    gameDisplay.blit(textsurface,(x,y))
+
+def DFSbutton(gameDisplay,msg,x,y,w,h,ic):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if click[0] == 1 and action != None:
+            action()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface  = myfont.render(msg, False ,( 0,0,0))
+    gameDisplay.blit(textsurface,(x,y))
+
+def BFS(grid,gridState, start, screen):
+    queue = collections.deque([[start]])
+    seen = set([start])
+   
+    while queue:
+       
+        path = queue.popleft()
+        
+        
+        x, y = path[-1]
+        grid[y][x] = 4
+        gridState[y][x].visted  == True
+      #  updateGrid(grid,screen)
+       # time.sleep(.300)
+        if gridState[y][x].isEndingPoint == True :
+            return path
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            if 0 <= x2 < 32 and 0 <= y2 < 19 and (x2, y2) not in seen and gridState[y][x].isWalled == False :
+                queue.append(path + [(x2, y2)])
+                seen.add((x2, y2))
+
+def drawPath(grid,path):
+    a = 0 
+  
---
+++ PyPathFinder.py
@@ -0,0 +1,144 @@
+import pygame
+from Functions import  cell, button, Resetbutton, intiGrid, updateGrid, Startbutton,  Endbutton,BFS, ReturnStartState
+
+#class to for cell info
+ 
+
+#creating the grid stae 
+gridState =[[cell() for j  in range(33) ] for i in range(20)]
+# Define some colors
+BLACK = (26, 8, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GREY = (169, 169, 169)  
+ALICE_BLUE = (240, 248, 255)
+
+
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 15
+HEIGHT = 15
+ 
+# This sets the margin between each cell
+MARGIN = 3
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+ # Append a cell
+ 
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid= []
+
+# Initialize pygame
+pygame.init()
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [600, 500]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+ 
+# Set title of screen
+pygame.display.set_caption("PATH FINDER ")
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+# Loop until the user clicks the close button.
+done = False
+startSet  = False
+endSet = False
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+intiGrid(grid,gridState, screen)
+
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # Set that location to one
+            
+            if ( column < 33 and row < 20): # cheecks if the click is within the constrints of the grid 
+
+                if(grid[row][column] == 0 and gridState[row][column].isWalled == False ): # making the walls toggable 
+                    grid[row][column] = 1
+                    gridState[row][column].isWalled = True
+                    print("Click ", pos, "Grid coordinates: ", row, column)
+
+                else:
+                    grid[row][column] = 0
+                    gridState[row][column].isWalled = False
+
+            if (  20 <= row <= 23  and  13 <= column <= 18 ):
+               intiGrid(grid,gridState, screen) # reset button can be fixed later   
+            if (  20 <= row <= 23  and  0 <= column <= 5 ):
+               Startbutton(event, grid,gridState)
+            if (  20 <= row <= 23  and  6 <= column <= 12 ):
+               Endbutton(event, grid,gridState)
+            if (  24 <= row <= 26  and  0 <= column <= 5 ):
+               start = ReturnStartState(gridState)
+               path = BFS(grid,gridState, start, screen)
+            
+                
+                  
+            print("Click ", pos, "Grid coordinates: ", row, column)
+            
+
+
+ 
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the grid
+    
+#add new color state while the grid changes 
+    updateGrid(grid,screen)
+    
+
+
+
+            
+
+ 
+    # Limit to 60 frames per second
+    #button art
+  
+    
+
+
+
+
+
+
+
+
+
+    StartButt = button( screen, 'Start', 5,370,100,50, GREEN, RED)
+    EndButt = button( screen, 'End', 120 ,370 ,100,50, GREEN, RED)
+   
+    button( screen,'RESET', 235,370,100,50,GREEN,RED)
+    QuitButt = button( screen, 'quit', 350 ,370 ,100,50, GREEN, RED,)
+
+    BFSButt = button( screen, 'BFS', 5 ,430 ,100,50, GREEN, RED)
+    DFSButt = button( screen, 'DFS', 120 ,430 ,100,50, GREEN, RED)
+    unButt = button( screen, 'Un', 235 ,430 ,100,50, GREEN, RED)
+
+   
+   
+   
+
+    
+
+    clock.tick(60)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
